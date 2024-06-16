e107cecdb2f610180a0dadc70b33de845d5f41eb
Elizaveta Razumenko <elizaveta.razumenko@pargoo.com>
2024-06-16 18:34:24 +0300

    add swagger and tests

---
+++ products-service/test/mock-data.ts
@@ -0,0 +1,74 @@
+import { Product } from "../types/types";
+
+export const mockProducts: Product[] = [
+  {
+    id: '1',
+    title: 'Margherita Pizza',
+    description: 'Classic pizza with tomato sauce, mozzarella, and basil.',
+    price: 8.99,
+    count: 10,
+  },
+  {
+    id: '2',
+    title: 'Pepperoni Pizza',
+    description: 'Spicy pepperoni with mozzarella and tomato sauce.',
+    price: 9.99,
+    count: 15,
+  },
+  {
+    id: '3',
+    title: 'BBQ Chicken Pizza',
+    description: 'Grilled chicken, BBQ sauce, red onions, and cilantro.',
+    price: 11.99,
+    count: 8,
+  },
+  {
+    id: '4',
+    title: 'Vegetarian Pizza',
+    description: 'Mixed vegetables, mozzarella, and tomato sauce.',
+    price: 9.49,
+    count: 12,
+  },
+  {
+    id: '5',
+    title: 'Hawaiian Pizza',
+    description: 'Ham, pineapple, mozzarella, and tomato sauce.',
+    price: 10.49,
+    count: 6,
+  },
+  {
+    id: '6',
+    title: 'Four Cheese Pizza',
+    description: 'A blend of mozzarella, cheddar, parmesan, and blue cheese.',
+    price: 10.99,
+    count: 9,
+  },
+  {
+    id: '7',
+    title: 'Buffalo Chicken Pizza',
+    description: 'Spicy buffalo chicken, mozzarella, and blue cheese dressing.',
+    price: 12.49,
+    count: 5,
+  },
+  {
+    id: '8',
+    title: 'Meat Lover\'s Pizza',
+    description: 'Pepperoni, sausage, ham, bacon, and mozzarella.',
+    price: 13.99,
+    count: 7,
+  },
+  {
+    id: '9',
+    title: 'Mushroom Pizza',
+    description: 'Fresh mushrooms, mozzarella, and garlic butter sauce.',
+    price: 9.99,
+    count: 10,
+  },
+  {
+    id: '10',
+    title: 'White Pizza',
+    description: 'Ricotta, mozzarella, parmesan, garlic, and olive oil.',
+    price: 11.49,
+    count: 8,
+  },
+]
--- products-service/test/products-service.test.ts
+++ products-service/test/products-service.test.ts
@@ -1,17 +1,79 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as ProductsService from '../lib/products-service-stack';
+import { handler as getProductListHandler } from '../lambda/getProductsList';
+import { handler as getProductsById } from '../lambda/getProductsById';
+import { mockProducts } from './mock-data';
+import { APIGatewayProxyEvent } from 'aws-lambda';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/products-service-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new ProductsService.ProductsServiceStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+jest.mock('../lambda/helpers', () => ({
+  getHeaders: jest.fn(() => ({ 'Content-Type': 'application/json' })),
+}));
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+
+describe('getProductsById handler', () => {
+     beforeEach(() => {
+        process.env.PRODUCTS = JSON.stringify(mockProducts);
+    });
+
+    afterEach(() => {
+        delete process.env.PRODUCTS;
+    });
+
+    it('should return the product if productId is found', async () => {
+      const event = { pathParameters: { id: '1' } } as unknown as APIGatewayProxyEvent;
+  
+      const result = await getProductsById(event);
+  
+      expect(result.statusCode).toBe(200);
+      expect(result.headers).toEqual({ 'Content-Type': 'application/json' });
+      expect(result.body).toBe(JSON.stringify(mockProducts[0]));
+    });
+  
+    it('should return 404 if product is not found', async () => {
+      const event = { pathParameters: { id: 'non-existent' } } as unknown as APIGatewayProxyEvent;
+  
+      const result = await getProductsById(event);
+  
+      expect(result.statusCode).toBe(404);
+      expect(result.headers).toEqual({ 'Content-Type': 'application/json' });
+      expect(result.body).toBe(JSON.stringify({ message: 'Product not found' }));
+    });
+
+    it('should return 404 if product is not found', async () => {
+      const event = { pathParameters: { id: 'non-existent' } } as unknown as APIGatewayProxyEvent;
+  
+      const result = await getProductsById(event);
+  
+      expect(result.statusCode).toBe(404);
+      expect(result.headers).toEqual({ 'Content-Type': 'application/json' });
+      expect(result.body).toBe(JSON.stringify({ message: 'Product not found' }));
+    });
+
+});
+
+
+describe('getProducts handler', () => {
+  beforeAll(() => {
+    process.env.PRODUCTS = JSON.stringify(mockProducts);
+  });
+
+  afterAll(() => {
+    delete process.env.PRODUCTS;
+  });
+
+  it('should return 200 and the list of products', async () => {
+    const result = await getProductListHandler();
+
+    expect(result.statusCode).toBe(200);
+    expect(result.headers).toEqual({ 'Content-Type': 'application/json' });
+    expect(result.body).toBe(JSON.stringify(mockProducts));
+  });
+
+  it('should return 200 and an empty list if no products are available', async () => {
+    delete process.env.PRODUCTS;
+
+    const result = await getProductListHandler();
+
+    expect(result.statusCode).toBe(200);
+    expect(result.headers).toEqual({ 'Content-Type': 'application/json' });
+    expect(result.body).toBe(JSON.stringify([]));
+  });
 });
